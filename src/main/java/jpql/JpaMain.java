a47835b8d7bc4092a95e416c16b2adce230945b9
liveforone <yc4852@gmail.com>
2022-03-14 11:34:48 +0900

    Initial commit

---
+++ src/main/java/jpql/JpaMain.java
@@ -0,0 +1,37 @@
+package jpql;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.List;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();  //트랜잭션 시작
+
+        try {  //에러가 없을시
+
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setAge(10);
+            em.persist(member);
+            
+            tx.commit();  //커밋해줌
+        } catch (Exception e) {  //에러발생하면 롤백
+            tx.rollback();
+        } finally {
+            em.clear();  //엔티티매니저 닫음
+        }
+        emf.close();
+    }
+}
